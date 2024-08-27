2e26080530e46a7034cb5ecd5485a12f0395bd75
rusov <rusov63@yahoo.com>
2024-08-27 23:06:38 +0400

    add unittest

--- app/sofa/tests/tests_calc_platelet.py
+++ app/sofa/tests/tests_calc_platelet.py
@@ -5,5 +5,17 @@ from app.sofa.handlers.calc_platelet import calculation_platelet
 
 class Test_calculation_platelet(unittest.TestCase):
 
-    def test_calculation(self):
-        self.assertGreaterEqual(calculation_platelet('> 151'), 0)
+    def test_returnZero(self):
+        self.assertEqual(calculation_platelet('> 151'), 0)  # Тромбоциты >= 151
+
+    def test_returnOne(self):
+        self.assertEqual(calculation_platelet('< 110'), 1)  # 101 < Тромбоциты <= 100
+
+    def test_returnTwo(self):
+        self.assertEqual(calculation_platelet('< 61'), 2)  # 51 < Тромбоциты <= 100:
+
+    def test_returnFree(self):
+        self.assertEqual(calculation_platelet('< 32'), 3)  # 21 < Тромбоциты <= 50
+
+    def test_returnFour(self):
+        self.assertEqual(calculation_platelet('< 12'), 4)  # 0 < Тромбоциты <= 20
