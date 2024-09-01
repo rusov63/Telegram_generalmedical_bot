c3729895afe9712a97dc2fd3067c1836ff4c4543
rusov <rusov63@yahoo.com>
2024-09-01 20:20:22 +0400

    upgrade docstring

--- app/skf/keyboard.py
+++ app/skf/keyboard.py
@@ -1,14 +1,24 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
 
+ONE = ('Женский')
+
+TWO = ('Мужской')
+
 def kb_skf() -> ReplyKeyboardMarkup:
     """
-    Клавиатура привязанная к функции cmd_skf (команда /skf).
-    :return: ReplyKeyboardMarkup
+    Создает клавиатуру для команды /skf.
+
+    Эта функция возвращает объект ReplyKeyboardMarkup, который содержит
+    кнопки для выбора пола: 'Женский' и 'Мужской'. Клавиатура
+    настроена так, чтобы быть компактной и исчезать после выбора.
+
+    :return: Объект ReplyKeyboardMarkup с кнопками выбора пола.
     """
+
     keyboard = ReplyKeyboardMarkup(
         keyboard=[[KeyboardButton(text="Женский"), KeyboardButton(text="Мужской")]],
 
-        input_field_placeholder='Пол: Женский - Мужской', resize_keyboard=True, one_time_keyboard=True)
+        input_field_placeholder='Выберите ответ', resize_keyboard=True, one_time_keyboard=True)
 
     return keyboard
