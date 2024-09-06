91a8ede1acc5964439baf3bc4f632e513e03f6cd
rusov <rusov63@yahoo.com>
2024-09-06 17:33:30 +0400

    supplemented the application: 1. docstring 2. caching 3. checking the information received from the user

--- app/skf/handlers/calc_GenAgeCreatinin.py
+++ app/skf/handlers/calc_GenAgeCreatinin.py
@@ -1,25 +1,39 @@
+import time
+from functools import lru_cache
+
+
+@lru_cache(maxsize=3)
 def calc_skf(gender: str, age: str, creatinin: str) -> int:
     """
-    Функция рассчитывающая "Скорость клубочкой фильтрации по формуле CKD-EPI".
+    Функция рассчитывающая "Скорость клубочковой фильтрации по формуле CKD-EPI".
+
+    Данная функция принимает пол, возраст и уровень креатинина пользователя и
+    вычисляет скорость клубочковой фильтрации (СКФ) по формуле CKD-EPI.
+    Формула учитывает различия в расчетах для мужчин и женщин, а также
+    диапазоны значений креатинина. Результат возвращается в виде округленного
+    значения, выраженного в мл/мин/1.73м².
+
     Формула взята из источника: https://www.invitro.ru/analizes/for-doctors/2368/28340/
     Онлайн калькулятор для проверки результатов:
     https://boris.bikbov.ru/2013/07/21/kalkulyator-skf-rascheta-skorosti-klubochkovoy-filtratsii/
     https://www.kidney.org/professionals/kdoqi/gfr_calculator
 
+    :param gender: Пол пользователя ('мужской' или 'женский').
+    :param age: Возраст пользователя в годах.
+    :param creatinin: Уровень креатинина в мкмоль/л.
     :return: Integer. Возвращает округленный результат по формуле.
+    Кэширование:
+    Функция использует декоратор '@lru_cache' для кэширования результатов.
+    Это означает, что если функция вызывается с теми же аргументами,
+    результат будет возвращен из кэша, что ускоряет выполнение и
+    уменьшает количество вычислений. Максимальный размер кэша установлен
+    на 3, что позволяет хранить результаты трех последних уникальных
+    вызовов функции.
     """
+
     ML_MIN = 'мл/мин/1.73м²'
 
-    if gender not in ['женский', 'мужской']:
-        return f'<b>Ошибка, повторите еще раз!</b>'
-
-    elif age.isalpha() or int(age) >= 100 or int(age) <= 14:
-        return f'<b>Ошибка, повторите еще раз!</b>'
-
-    elif creatinin.isalpha():
-        return f'<b>Ошибка, повторите еще раз!</b>'
-
-    elif gender == 'женский':
+    if gender in ('женский', 'жен'):
         if int(creatinin) <= 62:
             result = round(144 * (0.993 ** int(age)) * ((int(creatinin) / 88.4) / 0.7) ** (-0.328))
             return f'Скорость клубочковой фильтрации: <b>{result} {ML_MIN}</b>'
@@ -28,7 +42,7 @@ def calc_skf(gender: str, age: str, creatinin: str) -> int:
             result = round(144 * (0.993 ** int(age)) * ((int(creatinin) / 88.4) / 0.7) ** (-1.210))
             return f'Скорость клубочковой фильтрации: <b>{result} {ML_MIN}</b>'
 
-    elif gender == 'мужской':
+    elif gender in ('мужской', 'муж'):
         if int(creatinin) <= 80:
             result = round(141 * (0.993 ** int(age)) * ((int(creatinin) / 88.4) / 0.9) ** (-0.412))
             return f'Скорость клубочковой фильтрации: <b>{result} {ML_MIN}</b>'
@@ -37,8 +51,13 @@ def calc_skf(gender: str, age: str, creatinin: str) -> int:
             result = round(141 * (0.993 ** int(age)) * ((int(creatinin) / 88.4) / 0.9) ** (-1.210))
             return f'Скорость клубочковой фильтрации: <b>{result} {ML_MIN}</b>'
 
-# gender = 'мужской'
-# age = int(50)
-# creatinin = int(102)
-
+# gender = ('женский')
+# age = int(78)
+# creatinin = int(56)
+#
+# start_time = time.perf_counter()
 # print(calc_skf(gender, age, creatinin))
+# end_time = time.perf_counter()
+#
+# print(f"Время выполнения: {end_time - start_time:.8f} seconds")
+# print(calc_skf.cache_info())
