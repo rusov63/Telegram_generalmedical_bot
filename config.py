784fad08a327e2b17aad41974588f0c688abdb48
rusov <rusov63@yahoo.com>
2024-09-19 22:51:28 +0400

    Подключил базу хранения Redis

--- config.py
+++ config.py
@@ -6,6 +6,8 @@ from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 from asyncpg_lite import DatabaseManager
 
+from aiogram.fsm.storage.redis import RedisStorage
+
 load_dotenv()
 
 TOKEN = str(os.getenv('BOT_TOKEN'))
@@ -18,5 +20,11 @@ db_manager = DatabaseManager(db_url=os.getenv('PG_LINK'), deletion_password=os.g
 # инициируем объект бота, передавая ему parse_mode=ParseMode.HTML по умолчанию
 bot = Bot(TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
 
+# хранения данных FSM Redis
+redis_url = os.getenv('REDIS_URL')
+
+# используется для хранения данных конечного автомата состояний (FSM) в Redis.
+storage = RedisStorage.from_url(os.getenv('REDIS_URL'))
+
 # инициируем объект бота
-dp = Dispatcher()
+dp = Dispatcher(storage=storage)
